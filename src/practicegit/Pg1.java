c7111f02eaedf8821c92a9f0e8ed5592a265372b
admin <admin@192.168.0.7>
2023-11-21 19:31:20 +0530

    projectA was created

---
+++ src/practicegit/Pg1.java
@@ -0,0 +1,14 @@
+package practicegit;
+
+public class Pg1 {
+
+	public static void main(String[] args) {
+ int a=10;
+ String b="siva";
+ System.out.println(a);
+ System.out.println(b);
+		
+		
+	}
+
+}
